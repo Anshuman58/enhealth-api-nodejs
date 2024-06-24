5b2fcbbbe3f5937fb3d09b5279348a2aab76f1e6
Anshuman Dash <anshuman.smartters@gmail.com>
2024-06-24 21:45:00 +0530

    minor changes

--- src/socket_utils/handleConsultationEvent.ts
+++ src/socket_utils/handleConsultationEvent.ts
@@ -1,5 +1,4 @@
-import { HookContext } from '@feathersjs/feathers';
-
+import { HookContext } from "@feathersjs/feathers";
 
 const handleConsultationEvent = async (result: any, context: HookContext) => {
     const { app, data } = context;
@@ -8,15 +7,19 @@ const handleConsultationEvent = async (result: any, context: HookContext) => {
     const socketChannel: any = [];
 
     if ([2, 3, 4, 5].includes(status)) {
-        const { user, vendor } = result;
+        const { user, vendor,doctor } = result;
 
         if (user) {
-            const userId = 'name' in user ? user._id : user;
+            const userId = "name" in user ? user._id : user;
             socketChannel.push(app.channel(`userIds/${userId}`));
         }
         if (vendor) {
-            const vendorId = 'name' in vendor ? vendor._id : vendor;
+            const vendorId = "name" in vendor ? vendor._id : vendor;
             socketChannel.push(app.channel(`userIds/${vendorId}`));
+        }
+        if (doctor) {
+            const doctorId = "name" in doctor ? doctor._id : doctor;
+            socketChannel.push(app.channel(`userIds/${doctorId}`));
         }
         return socketChannel;
     }
