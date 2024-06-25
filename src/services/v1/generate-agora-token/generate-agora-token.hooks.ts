1100f503cd2cba040bdffcff5a72bf98a1183b5f
Anshuman Dash <anshuman.smartters@gmail.com>
2024-06-25 19:43:40 +0530

    minor changes

--- src/services/v1/generate-agora-token/generate-agora-token.hooks.ts
+++ src/services/v1/generate-agora-token/generate-agora-token.hooks.ts
@@ -1,25 +1,24 @@
-import { HooksObject } from '@feathersjs/feathers';
-import * as authentication from '@feathersjs/authentication';
-import FRequired from '../../../hooks/FRequired';
-import { disallow, iff } from 'feathers-hooks-common';
-import Permit from '../../../hooks/Permit';
-import setCreatedBy from '../../../hooks/SetCreatedBy';
-import ValidateBookingAndGenerateAgoraToken from './hooks/ValidateBookingAndGenerateAgoraToken';
+import { HooksObject } from "@feathersjs/feathers";
+import * as authentication from "@feathersjs/authentication";
+import FRequired from "../../../hooks/FRequired";
+import { disallow, iff } from "feathers-hooks-common";
+import Permit from "../../../hooks/Permit";
+import setCreatedBy from "../../../hooks/SetCreatedBy";
+import ValidateBookingAndGenerateAgoraToken from "./hooks/ValidateBookingAndGenerateAgoraToken";
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
 
 export default {
     before: {
-        all: [authenticate('jwt')],
+        all: [authenticate("jwt")],
         find: [],
         get: [],
         create: [
             Permit.or(Permit.DOCTOR, Permit.USER, Permit.VENDOR),
-            // FRequired('bookingId'),
-            iff(Permit.is(Permit.USER), setCreatedBy('user')),
-            iff(Permit.is(Permit.DOCTOR), setCreatedBy('doctor')),
-            iff(Permit.is(Permit.VENDOR), setCreatedBy('vendor')),
+            // FRequired("bookingId"),
+            iff(Permit.is(Permit.DOCTOR), setCreatedBy("doctor")),
+            iff(Permit.is(Permit.VENDOR), setCreatedBy("vendor")),
             ValidateBookingAndGenerateAgoraToken(),
         ],
         update: [disallow()],
