72fdf82780d1150ca082cd4a885c476f388352a7
Anshuman Dash <anshuman.smartters@gmail.com>
2024-07-16 17:25:14 +0530

    minor changes

--- src/services/v1/generate-agora-token/hooks/ValidateBookingAndGenerateAgoraToken.ts
+++ src/services/v1/generate-agora-token/hooks/ValidateBookingAndGenerateAgoraToken.ts
@@ -2,33 +2,33 @@ import { HookContext } from '@feathersjs/feathers';
 import { AgoraToken_POST } from '../Interface/AgoraTokenInterface';
 import { BadRequest, FeathersError } from '@feathersjs/errors';
 import { RtcRole, RtcTokenBuilder } from 'agora-token';
+import { consultationBookingPath } from '../../../../service_endpoints/services';
+import { consultationBookingStatus } from '../../../../db_services/v1/consultation/consultation-booking/Interface/ConsultationookingInterface';
 
 const ValidateBookingAndGenerateAgoraToken = () => async (context: HookContext) => {
     const { app, params } = context;
-    const { bookingId, user, doctor } = context.data as AgoraToken_POST;
+    const { bookingId, doctor } = context.data as AgoraToken_POST;
 
-    // const serviceDetails = await app
-    //     .service(serviceBookingPath)
-    //     .get(bookingId, {
-    //         query: {
-    //             ...params.query,
-    //             user: user || { $ne: null },
-    //             doctor: doctor || { $ne: null },
-    //             status: bookingStatus.ONGOING,
-    //             visitType: visitType.VIDEO_CALLING_CHAT,
-    //         },
-    //     })
-    //     .catch((e: FeathersError) => {
-    //         console.log(e);
-    //         throw new BadRequest('Consultation can not be started');
-    //     });
+    const serviceDetails = await app
+        .service(consultationBookingPath)
+        ._get(bookingId, {
+            query: {
+                ...params.query,
+                doctor: doctor || { $ne: null },
+                status: consultationBookingStatus.STARTED,
+            },
+        })
+        .catch((e: FeathersError) => {
+            console.log(e);
+            throw new BadRequest('Consultation can not be started');
+        });
     const { appId, appCertificate } = app.get('agora');
 
     const uid = 'doctor' in context.data ? 1 : 2;
     const role = RtcRole.PUBLISHER;
     const totalMilliseconds = 0.5 * 60 * 60 * 1000;
     const expireTime = Math.floor(Date.now() / 1000) + Math.floor(totalMilliseconds / 1000);
-    const channelName = 'channel_667710ec995e2771ce210614';
+    const channelName = `channel_${serviceDetails._id}`;
 
     const token = RtcTokenBuilder.buildTokenWithUid(
         appId,
