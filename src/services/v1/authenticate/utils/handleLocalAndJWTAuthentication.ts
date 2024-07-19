70c2023d7133f518f9ffac116b0b00b95a732216
Anshuman Dash <anshuman.smartters@gmail.com>
2024-07-19 22:31:10 +0530

    minor changes

--- src/services/v1/authenticate/utils/handleLocalAndJWTAuthentication.ts
+++ src/services/v1/authenticate/utils/handleLocalAndJWTAuthentication.ts
@@ -1,17 +1,11 @@
 /**
  * Created By Soumya(soumya\@smartters.in) on 10/17/2022 at 1:42 PM.
  */
-import { HookContext } from "@feathersjs/feathers";
-import { BadRequest, FeathersError } from "@feathersjs/errors";
-import { AuthenticationResult } from "@feathersjs/authentication";
-import {
-    User_GET,
-    UserRole,
-} from "../../../../db_services/v1/user/interfaces/UserInterfaces";
-import {
-    authenticationPath,
-    userPath,
-} from "../../../../service_endpoints/services";
+import { HookContext } from '@feathersjs/feathers';
+import { BadRequest, FeathersError } from '@feathersjs/errors';
+import { AuthenticationResult } from '@feathersjs/authentication';
+import { User_GET, UserRole } from '../../../../db_services/v1/user/interfaces/UserInterfaces';
+import { authenticationPath, userPath } from '../../../../service_endpoints/services';
 
 /**
  * Handle email password login with local strategy and jwt strategy verification.
@@ -19,7 +13,7 @@ import {
  * @returns - The accessToken and user object received for the given credentials.
  */
 const handleLocalAndJWTAuthentication = async (
-    context: HookContext
+    context: HookContext,
 ): Promise<{
     accessToken: string;
     authentication: AuthenticationResult;
@@ -44,7 +38,7 @@ const handleLocalAndJWTAuthentication = async (
                 return [];
             });
 
-        if (!res.length) throw new BadRequest("Invalid Credential");
+        if (!res.length) throw new BadRequest('Invalid Credential');
     }
 
     const authenticationService = app.service(authenticationPath);
@@ -59,8 +53,8 @@ const handleLocalAndJWTAuthentication = async (
             },
             {
                 ...params,
-                provider: "server",
-            }
+                provider: 'server',
+            },
         )
         .catch((e: FeathersError) => {
             throw e;
